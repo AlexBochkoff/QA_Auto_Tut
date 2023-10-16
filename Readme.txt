5dd116c35cbdc186f6e291f632815b869fc74015
AlexBochkoff <alexbochkoff@gmail.com>
2023-10-16 19:33:49 +0300

    Rearranging files within repo

--- Readme.txt
+++
@@ -1,8 +0,0 @@
-Changes #1 for commit on Main branch.
-
-Changes #2 for commit on Testing branch.
-
-Vim editor training was pretty cool!
-
-
-
---
+++ src/config/config.py
@@ -0,0 +1,5 @@
+class Config:
+    """Config class is responsible for storing framework and env configuration"""
+    
+    def __init__(self) -> None:
+        pass
