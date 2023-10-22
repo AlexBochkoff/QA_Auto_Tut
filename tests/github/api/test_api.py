7cba795601355f46268385aa2cf1d5f4c0bfa634
AlexBochkoff <alexbochkoff@gmail.com>
2023-10-22 15:36:46 +0300

    Config and test_api files updates

---
+++ tests/github/api/test_api.py
@@ -0,0 +1,16 @@
+from src.config.config_easy import Config_Easy
+from src.config.config_harder import config
+
+def test_api():
+    print(Config_Easy.request_timeout)
+    print(config.BASE_URL_UI)
+
+def test_api():
+    print(Config_Easy.request_timeout)
+
+def test_api():
+    print(Config_Easy.request_timeout)
+
+def test_api():
+    print(Config_Easy.request_timeout)
+
---
+++ tests/github/ui/test_ui.py
@@ -0,0 +1,9 @@
+import requests
+timeout = 29
+
+
+def test_api():
+    requests.get(url, timeout=timeout)
+
+def test_api():
+    requests.get(url, timeout=timeout)
