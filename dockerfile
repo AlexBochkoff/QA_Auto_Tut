b7e074fba14af71a97332f073656ca3d06f100b8
AlexBochkoff <alexbochkoff@gmail.com>
2023-12-06 16:11:05 +0200

    Last changes 2

---
+++ dockerfile
@@ -0,0 +1,20 @@
+# PYTHON
+FROM python:3.7-slim-bullseye
+
+#JAVA
+# FROM openjdk:22-slim-bullseye
+
+# install dependencies 
+# COPY requirements file ONLY
+RUN pip install pytest requests selenium pytest-html 
+
+# mkdri /tests && cd /tests
+WORKDIR /tests
+
+# stands for 
+# COPY EVERYTHING from LOCALFOLDER to CURRENT FODLER IN CONTAINER(which is /tests)
+COPY . .
+
+# specify command to run
+ENTRYPOINT [ "pytest" ]
+CMD ["--html=reports/report.html", "--self-contained-html"]
--- src/applications/github/ui/github_ui.py
+++ src/applications/github/ui/github_ui.py
@@ -1,5 +1,41 @@
-class GitHubUI:
+from src.applications.github.ui.base_page import BasePage
+from selenium.webdriver.common.by import By
+
+
+class GitHubUILoginPage(BasePage):
     """Current class contains every UI usage we use in tests"""
 
-    def __init__(self) -> None:
-        pass
+    def __init__(self, driver) -> None:
+        super().__init__(driver)
+
+    def navigate_to_page(self):
+        self.driver.get("https://github.com/login")
+        # waiters that some data exists
+        
+    def try_to_login(self, username, password):
+        login_fld = self.find_el(By.ID ,"login_field")
+        #waiter to be added
+
+        login_fld.send_keys(username)
+        
+        if self.get_text(login_fld) != username:
+            print("Username is entered incorrectry")
+            raise RuntimeError("Username is entered incorrectry")
+        
+        pass_fld = self.find_el(By.ID ,"password") 
+        #waiter to be added
+        pass_fld.send_keys(password)
+
+        # click button
+        login_fld = self.find_el(By.NAME ,"commit")
+        #waiter to be added
+
+        login_fld.click()
+
+    def check_login_error_message(self):
+        error_msg = self.find_el(By.ID ,"js-flash-container") # replace with different identifier e.g. Text of error
+
+        return error_msg is not None
+
+    def close_browser(self):
+        self.driver.quit()
